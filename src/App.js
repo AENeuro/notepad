8c9f726087f799d09872e725030894707de2c90a
Andrew Z <aeneuro@outlook.com>
2019-03-17 00:07:07 +0800

    Add refresh button, loading indicator

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { getNote, postNote } from "./servcies";
 import "./App.css";
 
 const App = () => {
-  const [submitting, setSubmitting] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
@@ -13,14 +13,17 @@ const App = () => {
   }, []);
 
   const handleGetNote = async () => {
+    setLoading(true);
+    setInputValue("Loading Data...");
     const content = await getNote();
+    setLoading(false);
     setInputValue(content);
   };
 
   const handleSubmit = async () => {
-    setSubmitting(true);
+    setLoading(true);
     const response = await postNote(inputValue);
-    setSubmitting(false);
+    setLoading(false);
     message.success("Submitted");
     console.log(response);
   };
@@ -39,14 +42,23 @@ const App = () => {
           onChange={handleInputChange}
           value={inputValue}
         />
-        <Button
-          loading={submitting}
-          type="primary"
-          style={{ marginTop: "1em" }}
-          onClick={handleSubmit}
-        >
-          Submit
-        </Button>
+        <div>
+          <Button
+            loading={loading}
+            type="primary"
+            style={{ marginTop: "1em" }}
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+          <Button className="refresh-button" onClick={handleGetNote}>
+            <i
+              className={
+                loading ? "fas fa-sync-alt spinning" : "fas fa-sync-alt"
+              }
+            />
+          </Button>
+        </div>
       </Card>
     </div>
   );
